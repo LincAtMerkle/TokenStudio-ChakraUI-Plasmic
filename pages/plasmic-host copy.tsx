62deb59d6fcc0b94f27bfde170cd1d5041f57b51
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-01-18 06:56:05 +1100

    sematic tokens in plasmic-host work

---
+++ pages/plasmic-host copy.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { PlasmicCanvasHost } from "@plasmicapp/host";
+import { ChakraProvider } from "@chakra-ui/react";
+import {registerAllComponents} from "./allComponents";
+import { theme as proTheme } from '../pro-package/src/index'
+import { extendTheme, theme as baseTheme } from '@chakra-ui/react'
+import '@fontsource/inter/variable.css'
+
+const themePro = extendTheme(
+  {
+    colors: { ...baseTheme.colors, brand: baseTheme.colors.yellow },
+  },
+  proTheme,  
+)
+
+registerAllComponents();
+
+export default function PlasmicHost() {
+  return (
+    <ChakraProvider theme={themePro}>
+      <PlasmicCanvasHost />
+    </ChakraProvider>
+  );
+}
